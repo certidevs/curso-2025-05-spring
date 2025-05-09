b9b01fdd0bc13e8c907f72a66a0105eda8ad11f2
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-09 11:19:58 +0200

    crear más productos y saveAll

--- src/main/java/com/certidevs/Main.java
+++ src/main/java/com/certidevs/Main.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.util.List;
+
 @SpringBootApplication
 public class Main {
 
@@ -22,11 +24,18 @@ public class Main {
         Producto manzana = new Producto("Manzana", 1.50, 100, true);
         Producto yogur = new Producto("Yogur", 2.00, 50, true);
         Producto avena = new Producto("Avena", 3.50, 30, true);
+        Producto queso = new Producto("Queso", 7.50, 20, true);
+        Producto aguacate = new Producto("Aguacate", 5.00, 0, false);
 
         // guardar productos
         productoRepository.save(manzana);
         productoRepository.save(yogur);
         productoRepository.save(avena);
+        productoRepository.save(queso);
+        productoRepository.save(aguacate);
+
+        // guardar productos de golpe
+        // productoRepository.saveAll(List.of(manzana, yogur, avena, queso, aguacate));
 
 
     }
