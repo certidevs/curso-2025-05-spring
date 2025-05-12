4f0c385bb5cf135840652deeeea5368e571a6b03
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-12 11:38:59 +0200

    probar métodos en Main

--- src/main/java/com/certidevs/Main.java
+++ src/main/java/com/certidevs/Main.java
@@ -24,7 +24,7 @@ public class Main {
         Producto manzana = new Producto("Manzana", 1.50, 100, true);
         Producto yogur = new Producto("Yogur", 2.00, 50, true);
         Producto avena = new Producto("Avena", 3.50, 30, true);
-        Producto queso = new Producto("Queso", 7.50, 20, true);
+        Producto queso = new Producto("Queso", 7.50, 100, true);
         Producto aguacate = new Producto("Aguacate", 5.00, 0, false);
 
         // guardar productos
@@ -58,12 +58,40 @@ public class Main {
         List<Producto> productosBaratos = productoRepository.findByPrecioLessThan(precioMaximo);
         System.out.println("Productos con precio menor que " + precioMaximo + ":");
         for (Producto producto : productosBaratos) {
-            System.out.println("\t " + producto.getNombre() + ": " + producto.getPrecio() + "euros");
+            System.out.println("\t " + producto.getNombre() + ": " + producto.getPrecio() + " euros");
         }
 
+        // probar boolean existsByNombreIgnoreCase(String nombre)
+        String nombreExistente = "YOGUR";
+        boolean existeProducto = productoRepository.existsByNombreIgnoreCase(nombreExistente);
+        System.out.println("¿Existe un producto llamado '" + nombreExistente + "'? " + existeProducto);
+        String nombreNoExistente = "pera";
+        boolean noExisteProducto = productoRepository.existsByNombreIgnoreCase(nombreNoExistente);
+        System.out.println("¿Existe un producto llamado '" + nombreNoExistente + "'? " +
+                (noExisteProducto ? "Sí" : "No"));
 
+        // probar long countByStock(Integer stock)
+        Integer stockBuscado = 100;
+        long cantidadProductos = productoRepository.countByStock(stockBuscado);
+        System.out.println("Cantidad de productos con stock de " + stockBuscado +
+                ": " + cantidadProductos);
 
+        // List<Producto> findByPrecioBetween(Double precioStart, Double precioEnd)
+        Double rangoPrecioMinimo = 1.00;
+        Double rangoPrecioMaximo = 4.00;
+        List<Producto> productosEnRango = productoRepository.findByPrecioBetween(rangoPrecioMinimo, rangoPrecioMaximo);
 
+        System.out.println("Productos con precio entre " + rangoPrecioMinimo + " euros y " +
+                rangoPrecioMaximo + " euros:");
+        if (productosEnRango.isEmpty()) {
+            System.out.println("No se han encontrado productos en este rango de precio.");
+        } else {
+            for (Producto producto : productosEnRango) {
+                System.out.println(producto.getNombre() + ": " + producto.getPrecio() + " euros");
+            }
+        }
+
+        System.out.println("Total de productos en el rango: " + productosEnRango.size());
 
     }
 }
