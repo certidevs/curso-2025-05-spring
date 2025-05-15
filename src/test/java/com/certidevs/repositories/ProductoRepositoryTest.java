3d8896f02ddb519d5b36ccb924aef1500966316a
alansastre <alan@certidevs.com>
2025-05-15 12:03:59 +0200

    Testing buscar y Optional

--- src/test/java/com/certidevs/repositories/ProductoRepositoryTest.java
+++ src/test/java/com/certidevs/repositories/ProductoRepositoryTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
@@ -47,19 +49,61 @@ class ProductoRepositoryTest {
         assertEquals(1L, ropa.getId());
     }
 
+    // CRUD - Create Read Update Delete
     @Test
+    @DisplayName("La operación Create de CRUD de categoría y producto")
     void guardarProductoConCategoriaTest() {
 
         Categoria ropa = new Categoria("cat2", "cat2");
-        Producto p1 = new Producto("p1", 19.99, 1, true, ropa);
+        categoriaRepository.save(ropa); // se genera un id
 
+        Producto p1 = new Producto("p1", 19.99, 1, true, ropa);
         productoRepository.save(p1);
 
         assertNotNull(p1);
-        assertNotNull(p1.getId());
+        assertNotNull(p1.getId()); // verifica que se ha generado un id al guardar
         assertNotNull(ropa);
-        assertNotNull(ropa.getId());
+        assertNotNull(ropa.getId()); // verifica que se ha generado un id guardar
     }
 
+    @Test
+    void buscarProductosTest() {
 
+        // 1. GUARDAR PRODUCTO
+        Producto p1 = new Producto("p1", 19.99, 1, true, null);
+        productoRepository.save(p1);
+        Long productId = p1.getId(); // id generado en base de datos
+
+
+        // 2. BUSCAR PRODUCTO
+        // Optional es un envoltorio que envuelve objeto que podría ser null
+        Optional<Producto> productoOptional = productoRepository.findById(productId);
+
+        boolean existeProducto = productoOptional.isPresent();
+        assertTrue(existeProducto);
+
+        Producto productoGuardado = productoOptional.get();
+        System.out.println(productoGuardado);
+        assertEquals(p1.getId(), productoGuardado.getId());
+    }
+
+    @Test
+    @DisplayName("Buscar producto que no existe, da un Optional vacío")
+    void buscarProductoNoExiste() {
+
+        Optional<Producto> productOpt = productoRepository.findById(999L);
+
+        assertFalse(productOpt.isPresent());
+        assertTrue(productOpt.isEmpty());
+
+        // Como el producto es null si intentamos acceder da error, es normal:
+        // String productNombre = productOpt.get().getNombre(); // Lanza una excepción java.util.NoSuchElementException: No value present
+        // System.out.println(productNombre);
+
+        // Forma segura:
+        if (productOpt.isPresent()) {
+            Producto producto = productOpt.get();
+            System.out.println(producto.getNombre());
+        }
+    }
 }
