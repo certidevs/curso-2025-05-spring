3bb026605a074a50eaf8c033d3fcc524c367a0ea
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-19 13:38:00 +0200

    crear método informacion en HelloController

--- src/main/java/com/certidevs/controllers/HelloController.java
+++ src/main/java/com/certidevs/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.certidevs.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 // clase controlador MVC, maneja peticiones HTTP
@@ -10,6 +11,16 @@ public class HelloController {
     @GetMapping("hola") // http://localhost:8080/hola
     public String saludar() {
         return "hello"; // nombre archivo html (hello.html)
+    }
+
+    @GetMapping("info")
+    public String informacion(Model model) {
+        // añadir datos al modelo
+        model.addAttribute("desarrollador", "M. del Olmo");
+        model.addAttribute("fecha", "19/05/2025");
+        model.addAttribute("version", "1.0");
+
+        return "info"; // nombre archivo html
     }
 
 }
