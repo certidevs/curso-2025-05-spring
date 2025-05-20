9d5afb45bfb77bcfbb3487d3da94b5cd3ece7925
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-20 10:03:39 +0200

    implementar endpoint para listar productos

--- src/main/java/com/certidevs/controllers/ProductoController.java
+++ src/main/java/com/certidevs/controllers/ProductoController.java
@@ -1,13 +1,16 @@
 package com.certidevs.controllers;
 
+import com.certidevs.entities.Producto;
 import com.certidevs.repositories.ProductoRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
 
 @Controller
 public class ProductoController {
-
-//    @Autowired
-//    private ProductoRepository productoRepository;
 
     private ProductoRepository productoRepository;
 
@@ -15,8 +18,14 @@ public class ProductoController {
         this.productoRepository = productoRepository;
     }
 
-//    public String listProductos() {
-//        List<Producto> productos = productoRepository.findAll();
-//        return null;
-//    }
+    // anotación que mapea las peticiones GET a la URL "/productos"
+    @GetMapping("/productos") // http://localhost:8080/productos
+    public String findAll(Model model) {
+        // crear una lista con todos los productos
+        List<Producto> productos = productoRepository.findAll();
+        model.addAttribute("productos", productos);
+
+        return "producto-list";
+    }
+
 }
