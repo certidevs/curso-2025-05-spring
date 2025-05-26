a6e219a5d56d919195bc78deda76faceeabf2a03
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-26 12:28:50 +0200

    método findById en CategoriaController

--- src/main/java/com/certidevs/controllers/CategoriaController.java
+++ src/main/java/com/certidevs/controllers/CategoriaController.java
@@ -5,8 +5,10 @@ import com.certidevs.repositories.CategoriaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class CategoriaController {
@@ -23,6 +25,19 @@ public class CategoriaController {
         model.addAttribute("categorias", categorias);
 
         return "categoria-list";
+    }
+
+    @GetMapping("/categorias/{id}") // http://localhost:8080/categorias/1
+    public String findById(Model model, @PathVariable Long id) {
+        Optional<Categoria> categoriaOpt = categoriaRepository.findById(id);
+
+        if (categoriaOpt.isPresent()) {
+            model.addAttribute("categoria", categoriaOpt.get());
+        } else {
+            model.addAttribute("error", "404 Categoría Not Found");
+        }
+
+        return "categoria-detail";
     }
 
 }
