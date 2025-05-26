94d039c7d3a4ad8f294b8f2ff9a22ebbf8a0ca7e
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-26 10:15:18 +0200

    método findAll en CategoriaController

--- src/main/java/com/certidevs/controllers/CategoriaController.java
+++ src/main/java/com/certidevs/controllers/CategoriaController.java
@@ -1,7 +1,12 @@
 package com.certidevs.controllers;
 
+import com.certidevs.entities.Categoria;
 import com.certidevs.repositories.CategoriaRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class CategoriaController {
@@ -10,6 +15,14 @@ public class CategoriaController {
 
     public CategoriaController(CategoriaRepository categoriaRepository) {
         this.categoriaRepository = categoriaRepository;
+    }
+
+    @GetMapping("/categorias") // http://localhost:8080/categorias
+    public String findAll(Model model) {
+        List<Categoria> categorias = categoriaRepository.findAll();
+        model.addAttribute("categorias", categorias);
+
+        return "categoria-list";
     }
 
 }
