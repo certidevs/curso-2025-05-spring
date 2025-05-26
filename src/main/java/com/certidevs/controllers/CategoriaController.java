c9e872faecbab731b070b1ba5d6cdb5ab5880713
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-26 09:33:48 +0200

    inyección de dependencias en CategoriaController

--- src/main/java/com/certidevs/controllers/CategoriaController.java
+++ src/main/java/com/certidevs/controllers/CategoriaController.java
@@ -1,6 +1,15 @@
 package com.certidevs.controllers;
 
+import com.certidevs.repositories.CategoriaRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class CategoriaController {
 
+    private CategoriaRepository categoriaRepository;
+
+    public CategoriaController(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
 
 }
