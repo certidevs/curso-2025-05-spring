2c82fbd453666384bb436978cf4d5e2303cf461d
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-05-28 11:58:26 +0200

    método saveForm en ProveedorController

--- src/main/java/com/certidevs/controllers/ProveedorController.java
+++ src/main/java/com/certidevs/controllers/ProveedorController.java
@@ -7,7 +7,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 import java.util.Optional;
@@ -64,5 +66,13 @@ public class ProveedorController {
         }
 
         return "proveedor/proveedor-form";
+    }
+
+    // procesar formulario (crear o actualizar)
+    @PostMapping("/proveedores")
+    public String saveForm(@ModelAttribute Proveedor proveedor) {
+        proveedorRepository.save(proveedor);
+
+        return "redirect:/proveedores";
     }
 }
