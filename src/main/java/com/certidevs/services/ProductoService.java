857f19b909a4a2a32304d520b95c30e4c752a255
M. del Olmo <122261059+MdelolmoCode@users.noreply.github.com>
2025-06-09 12:29:36 +0200

    crear lógica de negocio en ProductoService

--- src/main/java/com/certidevs/services/ProductoService.java
+++ src/main/java/com/certidevs/services/ProductoService.java
@@ -1,5 +1,6 @@
 package com.certidevs.services;
 
+import com.certidevs.dto.ProductoStats;
 import com.certidevs.entities.Producto;
 import com.certidevs.repositories.ProductoRepository;
 import lombok.RequiredArgsConstructor;
@@ -40,5 +41,98 @@ public class ProductoService {
         return productoRepository.findByNombreContainsIgnoreCase(nombre);
     }
 
-    // lógica de negocio
+    // LÓGICA DE NEGOCIO
+
+    // método privado que valida si un producto es válido
+    private boolean isValidProduct(Producto producto) {
+        return producto.getDisponible() != null && producto.getDisponible() // está disponible
+        && producto.getStock() != null && producto.getStock() > 0 // tiene stock
+        && producto.getPrecio() != null && producto.getPrecio() > 0; // tiene precio válido
+    }
+
+    // calcular estadísticas para productos de UNA categoría específica
+    public ProductoStats calculateStatsByCategoryId(Long id) {
+        // obtener productos de la categoría
+        List<Producto> productos = productoRepository.findByCategoria_Id(id);
+
+        // encontrar precio máximo
+        double maxPrice = 0.0;
+        for (Producto producto : productos) {
+            // sólo considerar productos válidos
+            if (isValidProduct(producto) && producto.getPrecio() > maxPrice) {
+                maxPrice = producto.getPrecio();
+            }
+        }
+
+        // encontrar precio mínimo
+        double minPrice = maxPrice; // incializar con el máximo encontrado
+        for (Producto producto : productos) {
+            if (isValidProduct(producto) && producto.getPrecio() < minPrice) {
+                minPrice = producto.getPrecio();
+            }
+        }
+
+        // calcular totales para la media
+        int countProducts = 0; // contador de unidades
+        double sumPrice = 0.0; // suma total ponderada por stock
+        for (Producto producto : productos) {
+            if (isValidProduct(producto)) {
+                countProducts += producto.getStock(); // suma unidades
+                sumPrice += producto.getPrecio() * producto.getStock(); // suma ponderada
+            }
+        }
+
+        // calcular precio medio ponderado
+        double meanPrice = 0.0;
+        if (sumPrice > 0 && countProducts > 0) {
+            meanPrice = sumPrice / countProducts; // media ponderada
+            meanPrice = Math.round(meanPrice * 100) / 100.0; // redondear a 2 decimales
+        }
+
+        // devolver DTO con las estadíscas calculadas
+        return  new ProductoStats(minPrice, maxPrice, meanPrice, countProducts);
+    }
+
+    // calcular estadísticas para todos los productos
+    public ProductoStats calculateGeneralStats() {
+        // obtener todos los productos
+        List<Producto> productos = productoRepository.findAll();
+
+        // encontrar precio máximo
+        double maxPrice = 0.0;
+        for (Producto producto : productos) {
+            // sólo considerar productos válidos
+            if (isValidProduct(producto) && producto.getPrecio() > maxPrice) {
+                maxPrice = producto.getPrecio();
+            }
+        }
+
+        // encontrar precio mínimo
+        double minPrice = maxPrice; // incializar con el máximo encontrado
+        for (Producto producto : productos) {
+            if (isValidProduct(producto) && producto.getPrecio() < minPrice) {
+                minPrice = producto.getPrecio();
+            }
+        }
+
+        // calcular totales para la media
+        int countProducts = 0; // contador de unidades
+        double sumPrice = 0.0; // suma total ponderada por stock
+        for (Producto producto : productos) {
+            if (isValidProduct(producto)) {
+                countProducts += producto.getStock(); // suma unidades
+                sumPrice += producto.getPrecio() * producto.getStock(); // suma ponderada
+            }
+        }
+
+        // calcular precio medio ponderado
+        double meanPrice = 0.0;
+        if (sumPrice > 0 && countProducts > 0) {
+            meanPrice = sumPrice / countProducts; // media ponderada
+            meanPrice = Math.round(meanPrice * 100) / 100.0; // redondear a 2 decimales
+        }
+
+        // devolver DTO con las estadíscas calculadas
+        return  new ProductoStats(minPrice, maxPrice, meanPrice, countProducts);
+    }
 }
